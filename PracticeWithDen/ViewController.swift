31530ab0e62d9c5fd83e86b10be8a12984c61e73
SemperSpirit <kaiten-aiko@mail.ru>
2024-03-07 12:45:28 +0300

    add custom view

--- PracticeWithDen/ViewController.swift
+++ PracticeWithDen/ViewController.swift
@@ -19,6 +19,13 @@ final class ViewController: UIViewController {
             return imageView
         }()
     
+    private lazy var customView: UIView = {
+            let view = UIView()
+            view.backgroundColor = .white
+            view.translatesAutoresizingMaskIntoConstraints = false
+            return view
+        }()
+    
 // MARK: - Functions -
 
     override func viewDidLoad() {
@@ -32,10 +39,15 @@ final class ViewController: UIViewController {
     
     private func setupView() {
         view.backgroundColor = UIColor(red: 0.987, green: 0.987, blue: 0.987, alpha: 1)
+        customView.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.07).cgColor
+        customView.layer.shadowOffset = CGSize(width: 0, height: 3)
+        customView.layer.shadowOpacity = 1
+        customView.layer.shadowRadius = 36
     }
     
     private func setupHierarchy() {
         view.addSubview(imageView)
+        view.addSubview(customView)
     }
     
     private func setupLayout() {
@@ -46,6 +58,14 @@ final class ViewController: UIViewController {
             imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -97),
             imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -433),
             imageView.heightAnchor.constraint(equalToConstant: 251)
+        ])
+        
+        NSLayoutConstraint.activate([
+            customView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -31),
+            customView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            customView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+            customView.widthAnchor.constraint(equalToConstant: 345),
+            customView.heightAnchor.constraint(equalToConstant: 270)
         ])
     }
 }
