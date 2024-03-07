16212b6e01f3e7c161a7260fee0985993db165f2
SemperSpirit <kaiten-aiko@mail.ru>
2024-03-07 12:34:17 +0300

    Initial Commit

---
+++ PracticeWithDen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeWithDen
+//
+//  Created by Kaiten Aiko on 07.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
